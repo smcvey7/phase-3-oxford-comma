f442a0ae73a0c53b4c7cbebb9f74a5d3c5995caf
Steven McVey <smcvey1210@gmail.com>
2022-12-07 13:15:17 -0800

    commit tree 7535eae8b3b603a14aec798d8585cc5688543c0f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    x=array.length-1
+  final = array[0...x].join(", ")
+  final += ", and #{array[x]}"
+  end
 end
+
+test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+p oxford_comma(test)
